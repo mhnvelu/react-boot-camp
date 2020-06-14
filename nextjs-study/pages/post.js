a119dc7f6f74b5e7e14022eae2c62f3ca3237bb4
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-14 12:52:18 +0100

    NextJs - withRouter, gitInitialProps

--- nextjs-study/pages/post.js
+++ nextjs-study/pages/post.js
@@ -3,20 +3,28 @@ import { withRouter } from "next/router";
 const Post = (props) => {
   console.log("******** RUNNING POST PAGE ***********");
   return (
-    <h2>Post id : {props.router.query.id}</h2>
-
-    // <>
-    //   {props.post.map((p) => (
-    //     <p key={p.id}>{p.body}</p>
-    //   ))}
-    // </>
+    <>
+      <h2>Commnets for Post #{props.router.query.id}</h2>
+      {props.post.map((p) => (
+        <Comment key={p.id} {...p} />
+      ))}
+    </>
   );
 };
 
-// Post.getInitialProps = async (context) => {
-//   let url = `https://jsonplaceholder.typicode.com/comments?postId=${context.query.id}`;
-//   let response = await axios.get(url);
-//   return { post: response.data };
-// };
+const Comment = ({ email, body }) => {
+  return (
+    <div>
+      <h5>{email}</h5>
+      <p>{body}</p>
+    </div>
+  );
+};
+
+Post.getInitialProps = async (context) => {
+  let url = `https://jsonplaceholder.typicode.com/comments?postId=${context.query.id}`;
+  let response = await axios.get(url);
+  return { post: response.data };
+};
 
 export default withRouter(Post);
