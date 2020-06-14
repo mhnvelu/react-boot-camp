4c8727aad48118c7c5148ab24e8a34632b8e5a41
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-14 12:28:25 +0100

    NextJs - withRouter

--- nextjs-study/pages/post.js
+++ nextjs-study/pages/post.js
@@ -1,23 +1,22 @@
 import axios from "axios";
+import { withRouter } from "next/router";
 const Post = (props) => {
   console.log("******** RUNNING POST PAGE ***********");
   return (
-    <>
-      {props.post.map((p) => (
-        <p key={p.id}>{p.body}</p>
-      ))}
-    </>
+    <h2>Post id : {props.router.query.id}</h2>
+
+    // <>
+    //   {props.post.map((p) => (
+    //     <p key={p.id}>{p.body}</p>
+    //   ))}
+    // </>
   );
 };
 
-Post.getInitialProps = async (context) => {
-  console.log(context.query);
-  console.log(context.pathname);
-  console.log(context.aspath);
-  let url = `https://jsonplaceholder.typicode.com/comments?postId=${context.query.id}`;
-  let response = await axios.get(url);
-  console.log(response.data);
-  return { post: response.data };
-};
+// Post.getInitialProps = async (context) => {
+//   let url = `https://jsonplaceholder.typicode.com/comments?postId=${context.query.id}`;
+//   let response = await axios.get(url);
+//   return { post: response.data };
+// };
 
-export default Post;
+export default withRouter(Post);
