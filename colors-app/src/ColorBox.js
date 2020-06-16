ca44d78f4f1f7cbdd4fdabc402fe8d7a3c9e181a
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-16 22:36:54 +0100

    Colors App - Styled ColorBox component

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,9 +1,16 @@
 import React from "react";
 import "./ColorBox.css";
 export default function ColorBox(props) {
+  const { name, color } = props;
   return (
-    <div className="ColorBox" style={{ background: props.color }}>
-      <span>{props.name}</span>
+    <div className="ColorBox" style={{ background: color }}>
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">MORE</span>
     </div>
   );
 }
