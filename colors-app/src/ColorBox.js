83905181bce072e498f4109efb5b9bd1a6c428ea
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-17 11:55:38 +0100

    Colors App - Added overlay animation

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 export default function ColorBox(props) {
   const { name, color } = props;
+  const [copied, setCopied] = useState(false);
+
+  function changeCopyState() {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3500);
+  }
+
   return (
     <div className="ColorBox" style={{ background: color }}>
+      <div
+        className={`copy-overlay ${copied ? "show" : ""}`}
+        style={{ background: color }}
+      />
+      <div className={`copy-msg ${copied ? "show" : ""}`}>
+        <h1>COPIED!</h1>
+        <p>{color}</p>
+      </div>
+
       <div className="copy-container">
         <div className="box-content">
           <span>{name}</span>
         </div>
-        <CopyToClipboard text={color}>
+        <CopyToClipboard text={color} onCopy={changeCopyState}>
           <button className="copy-button">Copy</button>
         </CopyToClipboard>
       </div>
