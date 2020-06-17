7612cd87713c1e4621b00ef8e29190ef76e73f13
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-17 22:37:30 +0100

    Colors App - Added Snackbar using Material UI Snackbar component

--- colors-app/src/Palette.js
+++ colors-app/src/Palette.js
@@ -2,33 +2,63 @@ import React, { useState } from "react";
 import ColorBox from "./ColorBox";
 import Navbar from "./Navbar";
 import "./Palette.css";
+import Snackbar from "@material-ui/core/Snackbar";
+import CloseIcon from "@material-ui/icons/Close";
+import IconButton from "@material-ui/core/IconButton";
 
 export default function Palette(props) {
   const [level, setLevel] = useState(200);
-  const [format, setFormat] = useState("hex");
+  const [format, setFormat] = useState({ value: "hex", open: false });
 
   function handleSliderChange(newLevel) {
     setLevel(newLevel);
   }
 
   function handleFormatChange(e) {
-    setFormat(e.target.value);
-    console.log(e.target.value);
+    setFormat({ value: e.target.value, open: true });
+  }
+
+  function handleSnackbarClose() {
+    setFormat({ ...format, open: false });
   }
 
   return (
     <div className="Palette">
       <Navbar
         level={level}
-        format={format}
+        format={format.value}
         handleLevelChange={handleSliderChange}
         handleFormatChange={handleFormatChange}
       />
       <div className="Palette-colors">
         {props.colors[level].map((color) => (
-          <ColorBox name={color.name} color={color[format]} />
+          <ColorBox name={color.name} color={color[format.value]} />
         ))}
       </div>
+
+      <Snackbar
+        anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+        open={format.open}
+        autoHideDuration={3000}
+        message={
+          <span id="message-id">
+            Format Changed To {format.value.toUpperCase()}
+          </span>
+        }
+        onClose={handleSnackbarClose}
+        ContentProps={{
+          "aria-describedby": "message-id",
+        }}
+        action={
+          <IconButton
+            size="small"
+            aria-label="close"
+            color="inherit"
+            onClick={handleSnackbarClose}>
+            <CloseIcon fontSize="small" />
+          </IconButton>
+        }
+      />
     </div>
   );
 }
