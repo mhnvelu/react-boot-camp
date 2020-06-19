ef6eee0455e3a83c257c0d1f7fa55672caa69329
Mohanavelu Subramanian <mhnvelu@gmail.com>
2020-06-19 15:59:19 +0100

    Colors App - Dynamic text color based on luminance using chroma-js

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./ColorBox.css";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import { Link } from "react-router-dom";
+import chroma from "chroma-js";
 export default function ColorBox(props) {
   const { name, color, paletteId, colorId, showMore } = props;
   const [copied, setCopied] = useState(false);
@@ -11,29 +12,38 @@ export default function ColorBox(props) {
     setTimeout(() => setCopied(false), 3500);
   }
 
+  const isDarkColor = chroma(color).luminance() <= 0.09;
+  const isLightColor = chroma(color).luminance() >= 0.6;
   return (
     <div className="ColorBox" style={{ background: color }}>
       <div
         className={`copy-overlay ${copied ? "show" : ""}`}
         style={{ background: color }}
       />
-      <div className={`copy-msg ${copied ? "show" : ""}`}>
+      <div
+        className={`copy-msg ${copied ? "show" : ""}  ${
+          isLightColor && " dark-text"
+        }`}>
         <h1>COPIED!</h1>
         <p>{color}</p>
       </div>
 
       <div className="copy-container">
         <div className="box-content">
-          <span>{name}</span>
+          <span className={isDarkColor && "light-text"}>{name}</span>
         </div>
         <CopyToClipboard text={color} onCopy={changeCopyState}>
-          <button className="copy-button">Copy</button>
+          <button className={`copy-button ${isLightColor && "dark-text"}`}>
+            COPY
+          </button>
         </CopyToClipboard>
       </div>
 
       {showMore && (
         <Link to={`/palette/${paletteId}/${colorId}`}>
-          <span className="see-more">MORE</span>
+          <span className={`see-more ${isLightColor && "dark-text"}`}>
+            MORE
+          </span>
         </Link>
       )}
     </div>
